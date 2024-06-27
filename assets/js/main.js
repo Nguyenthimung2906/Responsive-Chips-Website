3902c402752a74a6909694e37710cb6237d05185
@mungnt <mungnt@hivelab.com>
2024-06-27 13:58:50 +0700

    Toggle Menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,20 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.querySelector(".nav__menu"),
+  navToggel = document.querySelector(".nav__toggel"),
+  navClose = document.querySelector(".nav__close")
+
+  if(navToggel) {
+    navToggel.addEventListener("click", function() {
+      navMenu.classList.add('show__menu')
+    })
+  }
+
+  if(navClose) {
+    navClose.addEventListener("click", function() {
+      navMenu.classList.remove('show__menu')
+    })
+  }
+
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
@@ -7,10 +23,10 @@
 /*=============== SHADOW HEADER ===============*/
 
 
-/*=============== SWIPER FAVORITES ===============*/ 
+/*=============== SWIPER FAVORITES ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
